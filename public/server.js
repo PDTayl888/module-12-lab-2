f2fff4dff7c5cb4e087c8f65889037c46345fc6d
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-03-11 18:10:19 -0400

    added get

--- public/server.js
+++ public/server.js
@@ -3,3 +3,11 @@ const axios = require('axios');
 
 const app = express();
 const port = 3000;
+
+app.get('/api/funfact', async () => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
